f38e8ab1afd7fd05ff5fe162aabff7f992c60b79
xjanus12x <albertovillacarlos07@gmail.com>
2024-12-12 00:35:43 +0800

    chore: remove black and white color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          darkGray: "hsl(0, 0%, 55%)",
+          veryDarkGray: "hsl(0, 0%, 41%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
-
