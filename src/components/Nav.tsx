fae6ed67ae5bbd3b9c4fbf1b68c01e362779adc8
xjanus12x <albertovillacarlos07@gmail.com>
2024-12-14 22:48:30 +0800

    feat: update Nav component to utilize Framer Motion for smoother animations and refactor for responsive menu handling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,34 @@
 import companyLogo from "../assets/images/logo.svg";
 import hamburgerMenu from "../assets/images/icon-hamburger.svg";
+import { useSlidingNav } from "../context/SlidingNavProvider";
+import NavItems from "./NavItems";
+import { motion } from "framer-motion";
+
 export default function Nav() {
-  const toggleMenu = false;
+  const { toggleNav, onClick } = useSlidingNav();
   return (
     <nav>
-      <div className="flex justify-between items-center">
+      <motion.div
+        className="flex items-center justify-between"
+        initial={{ opacity: 0 }} // Starting state
+        whileInView={{ opacity: 1 }} // Animate when in view
+        transition={{ duration: 0.8, ease: "easeOut" }} // Smooth transition
+        viewport={{ once: true, amount: 0.5 }} // Trigger animation once, 30% in view
+      >
         <img src={companyLogo} aria-hidden={true} />
 
+        <NavItems className="hidden text-white md:flex md:gap-6" />
+
         <button
-          className=""
-          aria-label={toggleMenu ? "Open menu" : "Close menu"}
+          className="md:hidden"
+          aria-label="Open menu"
           aria-controls="menu"
-          aria-expanded={toggleMenu}
+          aria-expanded={toggleNav}
+          onClick={onClick}
         >
           <img src={hamburgerMenu} alt="Hamburger menu" aria-hidden />
         </button>
-      </div>
+      </motion.div>
     </nav>
   );
 }
