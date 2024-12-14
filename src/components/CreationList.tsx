33e37f88dd424aa1bba3fa9ab07542fb4cf85975
xjanus12x <albertovillacarlos07@gmail.com>
2024-12-14 22:39:20 +0800

    refactor: update prop value, delete button, convert into separate component and updated classNames

--- src/components/CreationList.tsx
+++ src/components/CreationList.tsx
@@ -14,65 +14,82 @@ import theCuriosityMobile from "../assets/images/mobile/image-curiosity.jpg";
 import theCuriousityDesktop from "../assets/images/desktop/image-curiosity.jpg";
 import makeItFisheyeMobile from "../assets/images/mobile/image-fisheye.jpg";
 import makeItFisheyeDesktop from "../assets/images/desktop/image-fisheye.jpg";
+import Button from "./Button";
+import { motion } from "framer-motion";
 
 export default function CreationList() {
   return (
-    <section >
-      <h2 className="text-5xl text-center uppercase josefin-sans mb-14">
-        Our creations
-      </h2>
+    <section>
+      <motion.div
+        className="mb-12 sm:flex sm:items-center sm:justify-between"
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        <h2 className="text-4xl text-center uppercase lg:text-5xl josefin-sans">
+          Our creations
+        </h2>
+        <Button className="hidden sm:inline sm:mt-0 sm:mx-0" />
+      </motion.div>
 
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+      <motion.div
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4"
+        initial={{ opacity: 0, y:50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        viewport={{ once: true, amount: 0.3 }}
+      >
         <CreationCard
           creationDesktopImg={deepEarthDesktop}
           creationMobileImg={deepEarthMobile}
           title="Deep Earth"
-          titleMaxWidth="4ch"
+          titleMaxWidth="max-w-[4ch]"
         />
         <CreationCard
           creationDesktopImg={nightArcadeDesktop}
           creationMobileImg={nightArcadeMobile}
           title="Night Arcade"
-          titleMaxWidth="5ch"
+          titleMaxWidth="max-w-[5ch]"
         />
         <CreationCard
           creationDesktopImg={soccerTeamVrDesktop}
           creationMobileImg={soccerTeamVrMobile}
           title="Soccer Team VR"
-          titleMaxWidth="8ch"
+          titleMaxWidth="max-w-[8ch]"
         />
         <CreationCard
           creationDesktopImg={theGridDesktop}
           creationMobileImg={theGridMobile}
           title="The Grid"
-          titleMaxWidth="3ch"
+          titleMaxWidth="max-w-[3ch]"
         />
         <CreationCard
           creationDesktopImg={fromUpAboveVrDesktop}
           creationMobileImg={fromUpAboveVrMobile}
           title="From Up Above VR"
-          titleMaxWidth="10ch"
+          titleMaxWidth="max-w-[10ch]"
         />
         <CreationCard
           creationDesktopImg={pocketBorealisDesktop}
           creationMobileImg={pocketBorealisMobile}
           title="Pocket Borealis"
-          titleMaxWidth="6ch"
+          titleMaxWidth="max-w-[6ch]"
         />
         <CreationCard
           creationDesktopImg={theCuriousityDesktop}
           creationMobileImg={theCuriosityMobile}
           title="The Curiosity"
-          titleMaxWidth="3ch"
+          titleMaxWidth="max-w-[3ch]"
         />
         <CreationCard
           creationDesktopImg={makeItFisheyeDesktop}
           creationMobileImg={makeItFisheyeMobile}
           title="Make It Fisheye"
-          titleMaxWidth="10ch"
+          titleMaxWidth="max-w-[10ch]"
         />
-      </div>
-      <button className="block px-14 py-3.5 mx-auto mt-10 text-xl uppercase border-2 border-black josefin-sans">See All</button>
+      </motion.div>
+      <Button className="sm:hidden" />
     </section>
   );
 }
@@ -91,20 +108,18 @@ function CreationCard({
   titleMaxWidth,
 }: CreationCardProps) {
   return (
-    <div className="relative">
+    <div className="relative before:absolute before:inset-0 before:bg-gradient-to-r before:from-black/80 before:to-transparent md-plus:before:bg-gradient-to-t md:shadow-md">
       <picture>
         <source media="(min-width:768px)" srcSet={creationDesktopImg} />
-        <img
-          src={creationMobileImg}
-          alt={title}
-          className="object-cover w-full h-auto mix-blend-darken "
-        />
+        <img className="min-w-full" src={creationMobileImg} alt={title} />
       </picture>
 
-      <span
-        className={`text-3xl uppercase  text-white max-w-[${titleMaxWidth}] josefin-sans left-6 bottom-6 absolute`}
-      >
-        {title}
+      <span className="absolute inset-0 grid p-6 text-xl text-white uppercase josefin-sans md-plus:text-2xl md-plus:p-10">
+        <span
+          className={`self-end ${titleMaxWidth} md-plus:self-end md-plus:justify-self-center md-plus:text-center md-plus:max-w-max md-plus:text-balance`}
+        >
+          {title}
+        </span>
       </span>
     </div>
   );
