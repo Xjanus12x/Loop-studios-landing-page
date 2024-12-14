bf90efc9be6f9ef80b2b3882ecb9f3f59214b83c
xjanus12x <albertovillacarlos07@gmail.com>
2024-12-14 22:52:08 +0800

    feat: Added hover effect to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
 export default function Button({ className }: ButtonProps) {
   return (
     <button
-      className={`block py-2 mx-auto mt-10 text-lg tracking-widest uppercase border-2 border-black px-14 alata-regular ${className}`}
+      className={`block py-2 mx-auto mt-10 text-lg tracking-widest uppercase border-2 border-black px-14 alata-regular hover:bg-black hover:text-white ${className}`}
     >
       See all
     </button>
