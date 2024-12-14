89dadad17fb53f3fdc572fc22ff3f1525976eae1
xjanus12x <albertovillacarlos07@gmail.com>
2024-12-14 22:08:36 +0800

    refactor: re-import and utilize updated AboutUs Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import CompanyIntroduction from "./components/CompanyIntroduction";
+import AboutUs from "./components/AboutUs";
 import CreationList from "./components/CreationList";
+import Footer from "./components/Footer";
 import HeroBanner from "./components/HeroBanner";
 import Nav from "./components/Nav";
+import SlidingNav from "./components/SlidingNav";
 
 function App() {
+  
+  
   return (
     <div>
       <HeroBanner>
         <Nav />
+        <SlidingNav />
       </HeroBanner>
       <main>
-        <div className="px-6 py-28 space-y-28">
-          <CompanyIntroduction />
-          <CreationList/>
+        <div className="px-6 py-24 space-y-28 lg:space-y-36 max-w-[75rem] mx-auto md-plus:px-12 lg:py-36">
+          <AboutUs />
+          <CreationList />
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
