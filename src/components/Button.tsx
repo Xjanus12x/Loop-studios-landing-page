636fcde155ea2d87f8e8aa775129a38684c1f152
xjanus12x <albertovillacarlos07@gmail.com>
2024-12-14 22:42:05 +0800

    feat: Created button component for re-usability

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+type ButtonProps = {
+  className: string;
+};
+export default function Button({ className }: ButtonProps) {
+  return (
+    <button
+      className={`block py-2 mx-auto mt-10 text-lg tracking-widest uppercase border-2 border-black px-14 alata-regular ${className}`}
+    >
+      See all
+    </button>
+  );
+}
